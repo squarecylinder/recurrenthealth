87c1813488b60a3e733e4a5ed6ad24e858815570
Square Cylinder <ziencck@gmail.com>
2021-01-11 21:41:55 -0500

    changed data structure for post

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@ $(document).ready(()=>{
 
     $("#submit-btn").click(function(event) {
         event.preventDefault();
+        let dataString = $('#contactForm').serializeArray();
+        console.log(dataString);
         const formData = {
         name: $("#Name").val(),
         email: $("#Email").val(),
@@ -14,8 +16,7 @@ $(document).ready(()=>{
         $.ajax({
             type: "POST",
             url: "./php/mailer.php",
-            dataType: "json",
-            data: JSON.stringify(formData)
+            data: 'name='+$('#Name').val()+'&email='+$('#Email').val()+'&company='+$('#Company').val()+'&phone='+$('#PhoneNumber').val()+'&message='+$('#message').val()
         }).done(function (data) {
             console.log(data);
             alert("It's OK!");
