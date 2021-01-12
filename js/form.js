2e371f907ba45efdfa0eb2c97d87944a2868e99d
Square Cylinder <ziencck@gmail.com>
2021-01-11 19:55:23 -0500

    adding php

--- js/form.js
+++ js/form.js
@@ -1,15 +1,24 @@
 $(document).ready(()=>{
+
     $("#submit-btn").on("click", function(event) {
         event.preventDefault();
+        const formData = {
+        name: $("#Name").val(),
+        email: $("#Email").val(),
+        company: $("#Company").val(),
+        phone: $("#PhoneNumber").val(),
+        message: $("#Message").val()
+        }
         console.log("submit was hit")
+        console.log(formData)
         $("#submitted").removeClass("hidden");
-        // $.ajax({
-        //     URL: somephp.url,
-        //     TYPE: "POST",
-        //     DATA: JSON.stringify(formData),
-        //     success: function(){
-        //         alert("form sent")
-        //     }
-        // })
+        $.ajax({
+            TYPE: "POST",
+            URL: "../php/mailer.php",
+            DATA: JSON.stringify(formData),
+            success: function(){
+                alert("form sent")
+            }
+        })
     })
 })
